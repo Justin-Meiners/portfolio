0a665c3a23d1bbd5a89b524861b3f8a8f4c11b3a
Justin-Meiners <6justin14@gmail.com>
2026-07-15 04:23:22 -0500

    hostname base issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react()],
   build: {
     // 98.css breaks the default lightningcss minifier, so skip CSS minify
